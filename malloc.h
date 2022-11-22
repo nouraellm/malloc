a954d0880cd64a0e98a13aaa9ac017f4b54148b5
ノウラ <nouraellm@gmail.com>
2022-11-22 23:53:40 +0100

    starting with struct

--- malloc.h
+++ malloc.h
@@ -1,6 +1,14 @@
 #ifndef FT_PRINTF_H
 #define FT_PRINTF_H
 
+char memory[20000];
+
+struct slice {
+ size_t size;
+ int free;
+ struct slice *next; 
+};
+
 void *_malloc(unsigned int size);
 
 #endif
