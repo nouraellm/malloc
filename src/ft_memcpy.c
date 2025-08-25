5d0fbc94675f1f5b89ba4184da147330a2b4f198
ノウラ <nea@odoo.com>
2025-08-26 00:28:43 +0200

    clean ft_strcpy and ft memcpy from tabs

--- src/ft_memcpy.c
+++ src/ft_memcpy.c
@@ -1,18 +1,18 @@
 #include "../includes/ft_malloc.h"
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+void *ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t	i;
+    size_t	i;
 
-	i = 0;
-	if (src == NULL && dst == NULL)
-		return (0);
-	if (src == dst)
-		return ((char*)dst);
-	while (i < n)
-	{
-		((char*)dst)[i] = ((char*)src)[i];
-		i++;
-	}
-	return (dst);
+    i = 0;
+    if (src == NULL && dst == NULL)
+        return (0);
+    if (src == dst)
+        return ((char*)dst);
+    while (i < n)
+    {
+        ((char*)dst)[i] = ((char*)src)[i];
+        i++;
+    }
+    return (dst);
 }
