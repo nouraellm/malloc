5f23cb8bc5c1d83d18ecb25cb10ea345b71bbe85
ノウラ <nea@odoo.com>
2026-01-22 01:05:42 +0100

    Improve error handling in ft_printf and streamline related functions

--- src/ft_debug.c
+++
@@ -1,7 +0,0 @@
-#include "../includes/ft_malloc.h"
-
-int is_debug_mode(void)
-{
-    char *dbg = getenv("MALLOC_DEBUG");
-    return (dbg && dbg[0] == '1');
-}
--- src/ft_thread.c
+++
@@ -1,5 +0,0 @@
-#include "../includes/ft_malloc.h"
-#include "ft_malloc_internal.h"
-
-t_zone *g_zones = NULL;
-pthread_mutex_t g_malloc_mutex = PTHREAD_MUTEX_INITIALIZER;
